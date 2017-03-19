779a46affd0b9f5e7a54868ff2ddcefe08bf8da2
Mark Lyen <mark@marklyen.com>
2017-03-18 19:36:33 -0700

    Added Web API calls for films controller. Links are on the main index page for testing.

---
+++ 2017MVCCoreMoviesApp/Controllers/API/FilmsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Models.DTO;
+using Movies.Web.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MoviesApp.Controllers.API
+{
+    [Route("api/films")]
+    public class FilmsController : Controller
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public FilmsController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<FilmDTO> Get()
+        {
+            return _movieRepository.GetAllFilms();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public FilmDTO Get(int id)
+        {
+            return _movieRepository.GetFilmDTO(id);
+        }
+    }
+}
--- 2017MVCCoreMoviesApp/Services/MovieRespository.cs
+++ 2017MVCCoreMoviesApp/Services/MovieRespository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Movies.Models.DTO;
 using Movies.Web.Models;
 
@@ -19,7 +20,9 @@ namespace Movies.Web.Services
         public IEnumerable<FilmDTO> GetAllFilms()
         {
             var returnVar = new List<FilmDTO>();
-            var films = _movieDbContext.Films.OrderBy(x => x.Title); 
+            var films = _movieDbContext.Films
+                .Include(x=>x.Director)
+                .OrderBy(x => x.Title); 
 
             foreach (var film in films)
             {
@@ -31,7 +34,7 @@ namespace Movies.Web.Services
 
         public FilmDTO GetFilmDTO(int filmid)
         {
-            return _dtoFactory.Map(_movieDbContext.Films.Single(x => x.Id == filmid));
+            return _dtoFactory.Map(_movieDbContext.Films.Include(x=>x.Director).Single(x => x.Id == filmid));
         }
     }
 }
